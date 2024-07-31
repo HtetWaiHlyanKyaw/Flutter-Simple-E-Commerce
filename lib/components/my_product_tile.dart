488864541f16d6159db4df9110b70d21d97eb459
Jaz <htetwaihlyankyaw27@gmail.com>
2024-07-31 21:55:04 +0630

    shop with products update

--- lib/components/my_product_tile.dart
+++ lib/components/my_product_tile.dart
@@ -1,10 +1,34 @@
 import 'package:e_commerce_ui/components/my_button.dart';
 import 'package:e_commerce_ui/models/product.dart';
+import 'package:e_commerce_ui/models/shop.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class MyProductTile extends StatelessWidget {
   final Product product;
   const MyProductTile({super.key, required this.product});
+
+  void addToCart(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: Text("Add this item to your cart?"),
+        actions: [
+          MaterialButton(
+            onPressed: () => Navigator.pop(context),
+            child: Text("Cancel"),
+          ),
+          MaterialButton(
+            onPressed: () {
+              Navigator.pop(context);
+              context.read<Shop>().addToCart(product);
+            },
+            child: Text("Yes"),
+          )
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -58,7 +82,9 @@ class MyProductTile extends StatelessWidget {
                   decoration: BoxDecoration(
                       color: Theme.of(context).colorScheme.secondary,
                       borderRadius: BorderRadius.circular(12)),
-                  child: IconButton(onPressed: () {}, icon: Icon(Icons.add))),
+                  child: IconButton(
+                      onPressed: () => addToCart(context),
+                      icon: const Icon(Icons.add))),
             ],
           ),
         ],
